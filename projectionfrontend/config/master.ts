ab83d4f2e7ec69e627c0b7d07d9ae1ea693cdf18
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-17 17:48:18 -0500

    deploy try

--- projectionfrontend/config/master.ts
+++ projectionfrontend/config/master.ts
@@ -1,9 +1,9 @@
 let config = { redirectUri: "", apiBaseUrl: "" };
 
-// config.redirectUri = "https://cs.mrg.com.pe/app-sec02-group03/oauth2/redirect";
-// config.apiBaseUrl = "https://api.cs.mrg.com.pe/api-sec02-group03";
+config.redirectUri = "https://cs.mrg.com.pe/app-sec02-group03/oauth2/redirect";
+config.apiBaseUrl = "https://api.cs.mrg.com.pe/api-sec02-group03";
 
-config.redirectUri = "http://localhost:3000/app-sec02-group03/oauth2/redirect";
-config.apiBaseUrl = "http://localhost:8080";
+// config.redirectUri = "http://localhost:3000/app-sec02-group03/oauth2/redirect";
+// config.apiBaseUrl = "http://localhost:8080";
 
 export default config;
