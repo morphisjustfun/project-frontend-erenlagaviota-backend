2deab5b315db95d69e661ecfb46c556fc7493048
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-17 13:31:08 -0500

    migrating

--- projectionfrontend/src/config/dev.ts
+++
@@ -1,6 +0,0 @@
-const redirectUri = "http://localhost:3000/oauth2/redirect";
-const apiBaseUrl = "http://localhost:8080";
-
-const exportConfig = {redirectUri, apiBaseUrl}
-
-export default exportConfig;
