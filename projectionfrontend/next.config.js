1aef4f32b50a421e57114d1ddb04589af10a7b25
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-17 16:55:21 -0500

    try deploy

--- projectionfrontend/next.config.js
+++ projectionfrontend/next.config.js
@@ -1,12 +1,9 @@
 const path = require("path");
 const withSass = require("@zeit/next-sass");
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-  cssModules: true,
-});
+
 module.exports = {
   /* Add Your Scss File Folder Path Here */
+    assetPrefix: ".",
   typescript: {
     ignoreBuildErrors: true,
   },
