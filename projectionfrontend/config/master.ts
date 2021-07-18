e96da7b994b5e1f4f3c46d39ffb77d201cf6ed65
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-18 04:23:46 -0500

    deploy aws WITH NEXT :)

--- projectionfrontend/config/master.ts
+++ projectionfrontend/config/master.ts
@@ -1,9 +1,15 @@
-let config = { redirectUri: "", apiBaseUrl: "" };
+import exportConfigDev from "./dev";
 
-config.redirectUri = "https://cs.mrg.com.pe/app-sec02-group03/oauth2/redirect";
+let config: typeof exportConfigDev = { redirectUri: "", apiBaseUrl: "" };
+
+// config.redirectUri = "https://cs.mrg.com.pe/app-sec02-group03/oauth2/redirect";
+// config.apiBaseUrl = "https://api.cs.mrg.com.pe/api-sec02-group03";
+
+// config.redirectUri = "http://localhost:3000/oauth2/redirect";
+// config.apiBaseUrl = "http://localhost:8080";
+//
+config.redirectUri = "http://projectionfrontend.s3-website.us-east-2.amazonaws.com/oauth2/redirect";
 config.apiBaseUrl = "https://api.cs.mrg.com.pe/api-sec02-group03";
 
-// config.redirectUri = "http://localhost:3000/app-sec02-group03/oauth2/redirect";
-// config.apiBaseUrl = "http://localhost:8080";
 
 export default config;
--- projectionfrontend/next.config.js
+++ projectionfrontend/next.config.js
@@ -1,19 +1,8 @@
 const path = require("path");
-const withSass = require("@zeit/next-sass");
 
 module.exports = {
   /* Add Your Scss File Folder Path Here */
-    webpack5: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  images: {
-    loader: "imgix",
-    path: "",
-    domains: ["lh3.googleusercontent.com"],
-  },
-  basePath: "/app-sec02-group03",
+  }
 };
