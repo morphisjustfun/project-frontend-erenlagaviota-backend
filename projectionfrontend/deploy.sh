ca6ac5e1f29ee40ab14aa483299d6acbcfd3ab7a
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-18 09:39:29 -0500

    script deploy

--- projectionfrontend/deploy.sh
+++ projectionfrontend/deploy.sh
@@ -1,5 +1,8 @@
-aws s3 cp ./out/ s3://projectionfrontend/ --recursive --exclude "*.html"
+cd out
 
-for file in $(find ./out/ -name '*.html' | sed 's|^\./||'); do
+aws s3 rm s3://projectionfrontend --recursive
+aws s3 cp ./ s3://projectionfrontend/ --recursive --exclude "*.html"
+
+for file in $(find ./ -name '*.html' | sed 's|^\./||'); do
     aws s3 cp ${file%} s3://projectionfrontend/${file%.*} --content-type 'text/html'
 done
