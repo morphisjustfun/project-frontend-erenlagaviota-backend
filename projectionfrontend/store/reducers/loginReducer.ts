99f401f59b9fc1ea9d9f71845d7cd44ce5ccb045
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-19 13:22:56 -0500

    fix

--- projectionfrontend/store/reducers/loginReducer.ts
+++ projectionfrontend/store/reducers/loginReducer.ts
@@ -1,30 +1,42 @@
 import { loginAction } from "../actions/loginAction";
 import { loginType, LoginState } from "../action-types/loginType";
 
-const initialState : LoginState = {
-    authenticated: {
+const initialState: LoginState = {
+  authenticated: {
+    authenticated: false,
+    waiting: true,
+  },
+  loading: false,
+  currentUser: 0,
+  imageUrl: "",
+};
+
+const loginReducer = (state: LoginState, action: loginAction) => {
+  if (state == undefined) {
+    state = initialState;
+  }
+  switch (action.type) {
+    case loginType.TOGGLE_LOADING:
+      return { ...state, loading: action.loading };
+    case loginType.LOG_OUT:
+      return {
+        ...state,
         authenticated: false,
-        waiting: true
-    },
-    loading: false,
-    currentUser: 0,
-    imageUrl: ''
-}
-
-const loginReducer = (state = initialState, action: loginAction) => {
-    switch (action.type){
-        case loginType.TOGGLE_LOADING:
-            return {...state, loading: action.loading}
-        case loginType.LOG_OUT:
-            return {...state, authenticated:false, currentUser: null, imageUrl: false}
-        case loginType.LOG_IN: 
-            return {...state, currentUser:action.currentUser, authenticated:action.authenticated, imageUrl: action.imageUrl}
-        case loginType.GET_IMAGEURL:
-            return {...state, imageUrl: action.imageUrl}
-        default:
-            return state
-    }
-}
-
+        currentUser: null,
+        imageUrl: false,
+      };
+    case loginType.LOG_IN:
+      return {
+        ...state,
+        currentUser: action.currentUser,
+        authenticated: action.authenticated,
+        imageUrl: action.imageUrl,
+      };
+    case loginType.GET_IMAGEURL:
+      return { ...state, imageUrl: action.imageUrl };
+    default:
+      return state;
+  }
+};
 
 export default loginReducer;
